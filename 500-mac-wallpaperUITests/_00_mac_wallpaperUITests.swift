7bb84f4b5876a94993c881e89dc2c79e5b5e6bf5
Marek Kadlčík <makadlcik@gmail.com>
2017-02-11 23:23:29 +0100

    add empty xcode project with gitignore

---
+++ 500-mac-wallpaperUITests/_00_mac_wallpaperUITests.swift
@@ -0,0 +1,36 @@
+//
+//  _00_mac_wallpaperUITests.swift
+//  500-mac-wallpaperUITests
+//
+//  Created by Marek Kadlčík on 11/02/2017.
+//  Copyright © 2017 Marek Kadlčík. All rights reserved.
+//
+
+import XCTest
+
+class _00_mac_wallpaperUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
