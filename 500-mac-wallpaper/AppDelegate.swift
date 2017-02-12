0fdec9582a2974d85035c86e975534a09693163d
Marek Kadlčík <makadlcik@gmail.com>
2017-02-12 17:34:02 +0100

    move menu controller to separate file

--- 500-mac-wallpaper/AppDelegate.swift
+++ 500-mac-wallpaper/AppDelegate.swift
@@ -11,17 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    let statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
-
-    func buttonClicked() {
-        print("Noot noot")
-    }
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let statusIcon = #imageLiteral(resourceName: "StatusIcon")
-        statusIcon.isTemplate = true
-        statusItem.image = statusIcon
-        statusItem.button!.action = #selector(buttonClicked)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ 500-mac-wallpaper/StatusMenuController.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+class StatusMenuController: NSObject {
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    override func awakeFromNib() {
+        let icon = #imageLiteral(resourceName: "StatusIcon")
+        icon.isTemplate = true
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+    }
+    
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+
+    
+}
